ee8f88d3f6ced2b9a941b0353e29ea196669fd6e
Ryan <ryan.trung@omnyfy.com>
2022-07-12 10:11:25 +0700

    init

---
+++ ActionLog/Observer/ActionLog.php
@@ -0,0 +1,24 @@
+<?php
+namespace Leo\ActionLog\Observer;
+
+class ActionLog implements \Magento\Framework\Event\ObserverInterface
+{
+    protected $url;
+
+    public function __construct(
+        \Magento\Framework\UrlInterface $url
+    ) {
+        $this->url = $url;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/action-log.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('Action log');
+        $logger->info('Module: ' . $this->url->getModuleName());
+        $logger->info('Action name: ' . $this->url->getActionName());
+        $logger->info('-------------------')
+    }
+}
